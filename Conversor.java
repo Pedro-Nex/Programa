bfbdc3a7a8dbe1f7fbf38c917c0749849a313ff2
Pedro Neves <pedro24319245@gmail.com>
2026-03-15 21:44:39 -0300

    ConversorJava

---
+++ Conversor.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class Conversor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final double DOLAR = 5.10;
+        final double EURO = 6.10;
+        final double LIBRA = 7.10;
+        final double PESO_ARGENTINO = 8.10;
+        final double PESO_CHILENO = 9.10;
+        final double PESO_COLOMBIANO = 10.10;
+        final double PESO_URUGUAIO = 11.10;
+        final double PESO_PARAGUAIO = 12.10;
+
+        System.out.println("*** Bem vindo ao Conversor de Moedas ***");
+
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+
+        if (idade < 18) {
+            System.out.println(nome + ", você não pode converter moedas.");
+            return;
+        }
+
+        System.out.print(nome + ", digite a quantidade de reais que você deseja converter: ");
+        double valor = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print(nome + ", digite a moeda para a qual você deseja converter: ");
+        String moeda = scanner.nextLine();
+
+        if (moeda.equalsIgnoreCase("DOLAR")) {
+            double valorConvertido = valor / DOLAR;
+            System.out.println("O valor convertido para Dólar é: " + String.format("%.2f", valorConvertido));
+        } else if (moeda.equalsIgnoreCase("EURO")) {
+            double valorConvertido = valor / EURO;
+            System.out.println("O valor convertido para Euro é: " + String.format("%.2f", valorConvertido));
+        } else if (moeda.equalsIgnoreCase("LIBRA")) {
+            double valorConvertido = valor / LIBRA;
+            System.out.println("O valor convertido para Libra é: " + String.format("%.2f", valorConvertido));
+        } else if (moeda.equalsIgnoreCase("PESO_ARGENTINO")) {
+            double valorConvertido = valor / PESO_ARGENTINO;
+            System.out.println("O valor convertido para Peso Argentino é: " + String.format("%.2f", valorConvertido));
+        } else if (moeda.equalsIgnoreCase("PESO_CHILENO")) {
+            double valorConvertido = valor / PESO_CHILENO;
+            System.out.println("O valor convertido para Peso Chileno é: " + String.format("%.2f", valorConvertido));
+        } else if (moeda.equalsIgnoreCase("PESO_COLOMBIANO")) {
+            double valorConvertido = valor / PESO_COLOMBIANO;
+            System.out.println("O valor convertido para Peso Colombiano é: " + String.format("%.2f", valorConvertido));
+        } else if (moeda.equalsIgnoreCase("PESO_URUGUAIO")) {
+            double valorConvertido = valor / PESO_URUGUAIO;
+            System.out.println("O valor convertido para Peso Uruguaio é: " + String.format("%.2f", valorConvertido));
+        } else if (moeda.equalsIgnoreCase("PESO_PARAGUAIO")) {
+            double valorConvertido = valor / PESO_PARAGUAIO;
+            System.out.println("O valor convertido para Peso Paraguaio é: " + String.format("%.2f", valorConvertido));
+        } else {
+            System.out.println("Moeda inválida");
+        }
+
+        scanner.close();
+    }
+}
