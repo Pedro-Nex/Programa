ca7a5aebc3979ba72e67b0621b1e58bbc01ca018
Pedro Neves <pedro24319245@gmail.com>
2026-03-22 15:40:55 -0300

    Correção do AdivinhaONum.java

--- AdivinhaONum.java
+++ AdivinhaONum.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 import java.util.Random;
-
 public class AdivinhaONum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
-
         int numeroSecreto = random.nextInt(100) + 1;
         int tentativas = 5;
         boolean acertou = false;
-
         System.out.println("**** Seja bem-vindo ao Jogo ****");
         System.out.println("Quero ver você acertar o número entre 1 e 100.");
-
         while (tentativas > 0 && !acertou) {
             System.out.println("\nVocê tem " + tentativas + " tentativas.");
             System.out.print("Digite um número: ");
@@ -22,7 +18,6 @@ public class AdivinhaONum {
                 scan.next();
                 continue;
             }
-
             int palpite = scan.nextInt();
             tentativas--;
 
@@ -34,13 +29,11 @@ public class AdivinhaONum {
                 System.out.println("O número secreto é MENOR que " + palpite);
             }
         }
-
         if (acertou) {
             System.out.println("\nParabéns! Você acertou o número secreto: " + numeroSecreto);
         } else {
             System.out.println("\nVocê perdeu! O número secreto era " + numeroSecreto);
         }
-
         scan.close();
     }
 }
